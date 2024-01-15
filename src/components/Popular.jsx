ed1fed8bf510b75e9de63eb03a82493af33dc166
Leeann Saengsavang <125524497+lsaengsavang@users.noreply.github.com>
2024-01-14 17:56:14 -0800

    Added code to fetch from API key, installed packages for styling

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -1,7 +1,35 @@
+import { useEffect, useState } from "react";
+
 function Popular() {
+
+  const [popular, setPopular] = useState([]);
+
+  useEffect(() => {
+    getPopular();
+  },[]);
+  // Empty array, to only run it when component gets mounted
+
+  // Fetching the recipes from Spoonacular API
+  const getPopular = async () => {
+    const api = await fetch (`https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=15`)
+    const data =await api.json();
+    console.log(data);
+    setPopular(data.recipes)
+  }
+
+
   return (
-    <div>Popular</div>
+    <div>
+      {popular.map((recipe) => {
+        return (
+          <div key={recipe.id}>
+            <p>{recipe.title}</p>
+          </div>
+        );
+      })}
+    </div>
   )
+  
 }
 
 export default Popular
